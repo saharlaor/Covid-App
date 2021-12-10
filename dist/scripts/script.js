93d93c4befb003837fd9fba3671aec01eb025548
saharoori <saharlaor@gmail.com>
2021-12-10 22:26:57 +0200

    Catch errors from API calls

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -60,8 +60,12 @@ const countries = {};
 
 // Functions
 async function request(url) {
-  const req = await axios.get(url);
-  return req.data;
+  try {
+    const req = await axios.get(url);
+    return req.data;
+  } catch (e) {
+    return e;
+  }
 }
 
 function clearArray(arr) {
@@ -71,31 +75,36 @@ function clearArray(arr) {
 }
 
 async function getContinentCountries(continent) {
-  const countriesData = await request(
-    `${PROXY_URL}${COUNTRIES_API_URL}${continent}`
-  );
-  console.log(countriesData);
-  countriesData.forEach((country) => {
-    continents[continent].push({
-      name: country.name.common,
-      code: country.cca2,
+  try {
+    const countriesData = await request(
+      `${PROXY_URL}${COUNTRIES_API_URL}${continent}`
+    );
+    console.log(countriesData);
+    countriesData.forEach((country) => {
+      continents[continent].push({
+        name: country.name.common,
+        code: country.cca2,
+      });
     });
-  });
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 async function getCountriesCovidData(continent) {
   return Promise.all(
     continents[continent].map((country) => {
-      return new Promise(async (resolve, reject) => {
+      return new Promise(async (resolve, _reject) => {
         try {
           const countriesData = await request(
             `${PROXY_URL}${COVID_API_URL}${country.code}`
           );
           console.log(countriesData.data.latest_data);
           countries[country.name] = countriesData.data.latest_data;
-          resolve();
         } catch (err) {
-          reject(err);
+          console.log(err);
+        } finally {
+          resolve();
         }
       });
     })
@@ -119,21 +128,31 @@ function updateGraph(continent) {
   clearArray(graphDeathsData);
   clearArray(graphRecoveredData);
   continents[continent].forEach((country) => {
+    if (!countries[country.name]) {
+      return;
+    }
     graphCountries.push(country.name);
     graphConfirmedData.push(countries[country.name].confirmed);
     graphCriticalData.push(countries[country.name].critical);
     graphDeathsData.push(countries[country.name].deaths);
     graphRecoveredData.push(countries[country.name].recovered);
   });
+  // TODO: Remove spinner loader here
+  continentGraph.update();
 }
 
 async function continentPicked(e) {
   const continent = e.target.value;
   if (!continents[continent].length) {
-    await getContinentCountries(continent);
-    await getCountriesCovidData(continent);
-    updateCountrySelect(continent);
-    updateGraph(continent);
+    try {
+      await getContinentCountries(continent);
+      await getCountriesCovidData(continent);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      updateCountrySelect(continent);
+      updateGraph(continent);
+    }
   } else {
     updateCountrySelect(continent);
     updateGraph(continent);
